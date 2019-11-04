fc2cd8c4a20212415a49fea76b3585a03cd802cc
Jason Davies <jason.davies@healthcaregateway.co.uk>
2019-11-04 18:54:05 +0000

    first commit

---
+++ src/main/java/com/github/jasondavies1/lastfimtimetracker/LastFimTimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.jasondavies1.lastfimtimetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastFimTimeTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastFimTimeTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jasondavies1/lastfimtimetracker/LastFimTimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jasondavies1.lastfimtimetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastFimTimeTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
